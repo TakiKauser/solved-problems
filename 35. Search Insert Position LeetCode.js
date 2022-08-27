d98527df298e33624d4d7a4876dd885a7e51f62b
TakiKauser <takikauserbadhon@gmail.com>
2022-08-27 16:46:05 +0600

    linear solution 35

--- 35. Search Insert Position LeetCode.js
+++ 35. Search Insert Position LeetCode.js
@@ -6,6 +6,16 @@
 // const nums = [1, 2, 4, 5, 6, 7, 9, 11];
 // const target = 10;
 
+const searchInsert2 = (nums, target) => {
+
+    for (let low = 0; low < nums.length; low++) {
+        if (nums[low] >= target) {
+            return low;
+        }
+    }
+    return nums.length;
+}
+
 const searchInsert = (nums, target) => {
     let low = 0;
     let high = nums.length - 1;
