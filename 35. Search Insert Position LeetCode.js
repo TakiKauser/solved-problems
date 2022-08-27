121eae81910ab0c9bdc760b2dd861ce6fd6e3ecf
TakiKauser <takikauserbadhon@gmail.com>
2022-08-27 16:31:17 +0600

    35. Search Insert Position LeetCode 35

---
+++ 35. Search Insert Position LeetCode.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+// const nums = [1, 2, 4, 5, 6, 7, 9, 11];
+// const target = 10;
+
+const searchInsert = (nums, target) => {
+    let low = 0;
+    let high = nums.length - 1;
+    while (low < high) {
+        let mid = Math.floor((high + low) / 2);
+        if (nums[mid] === target) return mid;
+
+        nums[mid] > target ? high = mid : low = mid + 1;
+    }
+
+    if (low === high) return (target <= nums[low]) ? low : low + 1;
+};
+// console.log(searchInsert(nums, target));
+
